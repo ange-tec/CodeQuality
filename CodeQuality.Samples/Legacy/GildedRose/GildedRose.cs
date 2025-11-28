9f87838de8c6781be9e6201cf50370cb07721145
Tr00d <guillaume.faas@live.fr>
2025-11-28 08:57:57 +0100

    refactor: Open/Closed principle to create items with their own logic to update their quality

--- CodeQuality.Samples/Legacy/GildedRose/GildedRose.cs
+++ CodeQuality.Samples/Legacy/GildedRose/GildedRose.cs
@@ -8,48 +8,8 @@ public class GildedRose(IList<Item> items)
     {
         foreach (var item in items)
         {
-            if (item.IsAgedBrie())
-            {
-                item.UpdateAgedBrieQuality();
-                continue;
-            }
-
-            if (item.IsBackstagePass())
-            {
-                item.UpdateQualityForBackstagePass();
-                continue;
-            }
-
-            if (item.IsSulfura())
-            {
-                continue;
-            }
-
-            this.UpdateQualityForRegularItem(item);
-        }
-    }
-
-    private void DecreaseItemQualityWithOne(Item item)
-    {
-        item.Quality -= 1;
-    }
-
-    private bool IfQualityHigherThanZero(Item item)
-    {
-        return item.Quality > 0;
-    }
-
-    private void UpdateQualityForRegularItem(Item item)
-    {
-        if (this.IfQualityHigherThanZero(item))
-        {
-            this.DecreaseItemQualityWithOne(item);
-        }
-
-        item.SellIn -= 1;
-        if (item.SellIn < 0 && this.IfQualityHigherThanZero(item))
-        {
-            this.DecreaseItemQualityWithOne(item);
+            var parsedItem = Item.Parse(item);
+            parsedItem.UpdateQuality();
         }
     }
 }
